fbcebb376457d4d404561c3d4ec82a8cd7a8c1fd
Oleksandr Melnyk <melnykoleksandr724@gmail.com>
2025-12-21 15:45:07 +0100

    hw-3

--- js/home-work.js
+++ js/home-work.js
@@ -1,4 +1,4 @@
-// /**Задача 1. Замовлення дроїдів
+// ** HW-02 Задача 1. Замовлення дроїдів
 // * Станція з продажу ремонтних дроїдів готова до запуску, залишилося написати
 // * програмне забезпечення для відділу продажів.
 // * Оголоси функцію makeTransaction(quantity, pricePerDroid, customerCredits),
@@ -32,7 +32,7 @@
 // console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
 // console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
 
-// /**Задача 2. Форматування повідомлення
+// ** HW-02 Задача 2. Форматування повідомлення
 // *Оголоси функцію formatMessage(message, maxLength), яка приймає рядок (параметр message)
 // * та перевіряє його довжину відповідно до заданої максимальної довжини (параметр maxLength).
 // * Доповни код функції таким чином, що:
@@ -54,7 +54,7 @@
 // console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
 // console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
 
-// /**Задача 3. Перевірка спаму
+// ** HW-02 Задача 3. Перевірка спаму
 //  * Функція checkForSpam(message) приймає рядок (параметр message),
 //  * перевіряє його на вміст заборонених слів spam і sale, і повертає результат перевірки.
 //  * Слова в рядку параметра message можуть бути в довільному регістрі, наприклад SPAM або sAlE.
@@ -77,7 +77,7 @@
 // console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
 // console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
 
-// /**Задача 4. Доставка товару
+// ** HW-02 Задача 4. Доставка товару
 //  * Оголоси функцію getShippingCost(country),
 //  * яка повинна перевіряти можливість доставки товару в країну користувача (параметр country)
 //  * і повертати повідомлення про результат. Обов'язково використовуй інструкцію switch.
@@ -118,3 +118,68 @@
 // console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
 // console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
 // console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
+
+// ** HW-03 Задача 1. Генератор slug
+// * Напиши функцію slugify(title), яка приймає заголовок статті,
+// * параметр title і повертає slug, створений із цього рядка.
+// * Значенням параметра title будуть рядки, слова яких розділені лише пробілами.
+// * Усі символи slug повинні бути в нижньому регістрі.
+// * Усі слова slug повинні бути розділені тире.
+// * Візьми код нижче і встав після оголошення своєї функції для перевірки коректності
+// * /її роботи. У консоль будуть виведені результати її роботи.
+
+// function slugify(title) {
+//   return title.trim().toLowerCase().split(" ").join("-");
+// }
+// console.log(slugify("  Arrays for beginners  ")); // "arrays-for-beginners"
+// console.log(slugify("English for developer")); // "english-for-developer"
+// console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+// console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
+// ** HW-03 Задача 2. Композиція масивів
+// * Напиши функцію під назвою makeArray, яка приймає три параметри:
+// *  firstArray (масив), secondArray (масив) і maxLength (число).
+// * Функція повинна створювати новий масив, який містить усі елементи з firstArray,
+// * а потім усі елементи з secondArray.
+// * Якщо кількість елементів у новому масиві перевищує maxLength,
+// * функція повинна повернути копію масиву з довжиною maxLength елементів.
+// * В іншому випадку функція повинна повернути весь новий масив.*/
+
+// function makeArray(firstArray, secondArray, maxLength) {
+//   const newArray = firstArray.concat(secondArray);
+//   return newArray.slice(0, maxLength);
+// }
+// console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+// console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+// console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
+
+// ** HW-03 Задача 3. Фільтрація масиву чисел
+// * Напиши функцію filterArray(numbers, value), яка приймає масив чисел (numbers)
+// * та значення (value) як параметри. Функція повинна повертати новий масив
+// * лише тих чисел із масиву numbers, які більші за значення value.
+// * Усередині функції:
+// * Створи порожній масив, у який будеш додавати підходящі числа.
+// * Використай цикл для ітерації кожного елемента масиву numbers.
+// * Використовуй умовний оператор if усередині циклу для перевірки кожного елемента
+// * и додавання до свого масиву.
+// * Поверни свій новий масив з підходящими числами як результат.
+//  */
+
+// function filterArray(numbers, value) {
+//   const total = [];
+//   for (let i = 0; i < numbers.length; i++) {
+//     const element = numbers[i];
+//     if (element > value) {
+//       total.push(element);
+//     }
+//   }
+//   return total;
+// }
+// console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+// console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+// console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+// console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+// console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
